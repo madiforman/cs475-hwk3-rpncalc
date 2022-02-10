ef2272dc2b3839aaa8ead30c9a56015d1e4322a3
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2022-02-10 15:02:46 -0500

    almost done

--- main.c
+++ main.c
@@ -1,33 +1,108 @@
 /*
  ============================================================================
- Name        : PolishSol.c
- Author      : 
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
+ Name        : Reverse Polish Notation Calculator
+ Author      : Madison Sanchez-Forman
+ Version     : 2.9.22
+ Description : Evaluates input RPN expressions using stack based structure
  ============================================================================
  */
 
 #include <stdio.h>
 #include <math.h>
 #include <string.h>
+#include <ctype.h>
+
 #include "stack.h"
 #include "polish.h"
 
 #define MAX_EQ_LEN 100 /* maximum RPN expression input length */
-
-int main()
-{
-	// char eqn[MAX_EQ_LEN];	 // string to store the input RPN expression
-	// char resp[MAX_EQ_LEN]; // string to store the user's response "y" or "n"
-
-	// /* TODO */
-
-	// printf("Exiting...\n");
-	// return 0;
-	initalize();
-	push(11.0);
-	push(40);
-	printf("The top is: %lf \n", get_top());
-	return 0;
+/**
+ * @brief Returns 1/0 if input char is a valid operator [+,-,*,/]
+ * 
+ * @param c possible operator
+ * @return int - returns 1 if operator found 0 if not
+ */
+int is_operator(char c){
+	if(c=='+' ||c=='-'||c=='/'||c=='*')
+	{ return 1; } 
+	else { return 0;}
 }
+
+/**
+ * @brief prompt user if they would like to continue evaluating expressions
+ * 
+ * @return char - first index of users response
+ */
+char get_menu_option(){
+	char resp[MAX_EQ_LEN];
+	printf("Evaluate another? (y/n): ");
+	fgets(resp, sizeof(resp),stdin);
+	return resp[0]; 			/*since only valid responses are 'y'/'n', we only ever care about the first index */
+}
+
+/**
+ * @brief parse and evaluate does the bulk of stack manipulation and expression evaluation
+ * 
+ */
+void parse_and_evaluate(){
+  char eqn[MAX_EQ_LEN];
+  char *token;
+  int flag = 0; 				/*used for error handling*/
+  stack *head = NULL;
+  printf("Evaluate an equation (in RPN): \n");
+  fgets(eqn, sizeof(eqn), stdin);
+
+  token = strtok(eqn, " \t");  	/*break expression up by whitespace*/
+  while(token!=NULL){
+	if(isalnum(*token)){        /*if is alphanumeric*/
+		double num = atof(token);
+		push(&head, num);				/*convert and push on stack*/
+	}else if(is_operator(*token)){
+		if(size()<2){			
+			printf("Error: insufficient operands. \n");
+			flag = 1;
+		} else {
+			 evaluate(*token, &head); 			/*evaluate will solve whatevers on the stack if size is less than two*/
+			}
+	} else if(!is_operator(*token)){ 	/*if is operator didnt return true, then its not a valid operator*/
+		printf("Error: operator %c unrecognized. \n", *token);
+		flag = 1;
+		break;
+	} 
+	token = strtok(NULL, " ");
+  } 
+
+  if(flag==1){ /*if another error previously went off*/
+  flag = 0; 
+  }else {
+	double result = pop(&head); /*result will be NaN if something went wrong in evaluate*/
+    if(size()>1 && !isnan(result)){
+	printf("Error: too many operands entered.\n");
+	} else if(!isnan(result)){
+	printf("%lf\n", result);	
+	} 
+  }
+  
+
+}
+int main()
+{ 
+  //char eqn[MAX_EQ_LEN];
+  char c;
+  //double result = 0;
+  //int flag = 0;
+  do{
+	parse_and_evaluate();
+	c = get_menu_option();
+	switch(c){
+		case 'y':
+		break;
+		case 'n':
+		printf("Exiting...\n");
+		break;
+		default: 
+		printf("Uknown option %c. Try again. \n", c);
+	}
+  }while(c!='n');
+  return 0;
+  }
--- stack.h
+++ stack.h
@@ -1,49 +1,80 @@
 /*
- * stack.h
- *
- *  Created on: Jun 25, 2015
- *      Author: dchiu
+ ============================================================================
+ Name        : Stack.h - Linked List based stack structure
+ Author      : Madison Sanchez-Forman
+ Version     : 2.9.22
+ Description : uses a stack of doubles to build and evaluate RPN expressions
+ ============================================================================
  */
 #include <stdlib.h>
 #ifndef STACK_H_
 #define STACK_H_
 
-// TODO: declaration of stack struct and its functions go here
-struct node{
-    double data;
-    struct node *next;
+int node_count;                     /*increments/decrements with push and pop*/
+typedef struct stack stack;
+/*typedef to make referencing simpler in the rest of code*/
+
+struct stack
+{
+    double data; 
+    struct stack *next;
 };
-typedef struct node node;
-node *top;
-int node_count;
 
-void initalize(){
-top = NULL;
+
+
+void push(stack **head, double val)
+{
+stack *node = malloc(sizeof(stack));
+node->data=val;
+node->next=*head;
+*head=node;
+node_count++;
 }
 
-void push(double val){
-    node *temp;
-    temp = malloc(sizeof(node));
-    temp->data = val;
-    temp->next = top;
-    top = temp;
-    node_count++;
+double pop(stack **head)
+{
+    stack *temp=*head;
+    double val=temp->data;
+    *head=temp->next;
+    free(temp);
+    node_count--;
+    return val;
 }
-double pop(){
-node *temp;
-double val;
-temp = top;
-val = temp -> data;
-top = top->next;
-free(temp);
-node_count--;
-return val;
-}
+
 int size(){
 return node_count;
 } 
-double get_top(){
-    return top->data;
+void evaluate(char c, stack **head){
+    double result, op2;
+    switch(c){
+        case '+':
+            result = pop(head) + pop(head);
+            break;
+        case '*':
+            result = pop(head) * pop(head);
+            break;
+        case '-':
+            op2 = pop(head);
+            result = (pop(head)-op2);
+            break;
+        case '/':
+            op2 = pop(head);
+            if(op2== 0){
+                printf("Error: divide-by-zero.\n");
+                result = NAN;
+                break;
+            } else{
+                result = pop(head)/op2;
+                break;
+            }
+        default:
+        printf("Error: operator %c unrecognized. \n", c);
+        result = NAN;
+        break;
+    }
+    push(head, result);
 }
+
+
 
 #endif /* STACK_H_ */
